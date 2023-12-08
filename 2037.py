92bc85af53872c5f7cfe533c48146a3752f8e635
Joffily Ferreira <joffilyferreira@gmail.com>
2023-12-08 18:07:42 -0300

    Adds sorting problems

---
+++ 2037.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        count = 0
+        seats.sort()
+        students.sort()
+
+        for i in range(len(seats)):
+            count += abs(seats[i] - students[i])
+
+        return count
